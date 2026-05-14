238bdbc1ffc619324e9a3cf9e92d325e4923fbb8
Xvitalnow <xvitalnow@gmail.com>
2026-05-14 08:55:28 +0000

    update backend URL configuration and add .env to .gitignore

--- client/app/lib/config/url.js
+++ client/app/lib/config/url.js
@@ -1,11 +1,13 @@
 function getBackendURLOnTheBasisOfEnvironment() {
+  const proUrl= "https://xvital.render.com/api";
+  const devUrl= "https://shiny-enigma-969r9vv6wj4wc7v4j-5000.app.github.dev/api";
   if (process.env.NEXT_PUBLIC_IS_PRODUCTION === "true") {
     // In production environment, return the production backend URL
-    const productionURL = process.env.NEXT_PUBLIC_BACKEND_URL_PRODUCTION;
+    const productionURL = proUrl;
     return productionURL;
   } else {
     // In development environment, return the development backend URL
-    const developmentURL = process.env.NEXT_PUBLIC_BACKEND_URL_DEVELOPMENT;
+    const developmentURL = devUrl;
     return developmentURL;
   }
 }
