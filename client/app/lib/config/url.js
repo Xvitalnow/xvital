b0a7b5e74e3f7e82eb03bae350022db8a137f545
Xvitalnow <xvitalnow@gmail.com>
2026-05-19 06:34:49 +0000

    fix: correct casing in backend URL constant declaration

--- client/app/lib/config/url.js
+++ client/app/lib/config/url.js
@@ -1,5 +1,5 @@
-// export const backendURL = "https://studious-meme-69p55wgq659rc44vw-5000.app.github.dev/api";
-export const backendURL = "https://xvital.render.com/api";
+// export const BackendURL = "https://studious-meme-69p55wgq659rc44vw-5000.app.github.dev/api";
+export const BackendURL = "https://xvital.render.com/api";
 
 
 
