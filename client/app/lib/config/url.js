ee355e96ba809d06475e307814681c9176b5f46e
Xvitalnow <xvitalnow@gmail.com>
2026-05-19 06:28:10 +0000

    refactor: streamline backend URL configuration by removing environment-based function

--- client/app/lib/config/url.js
+++ client/app/lib/config/url.js
@@ -1,18 +1,5 @@
-function getBackendURLOnTheBasisOfEnvironment() {
-  const proUrl= "https://xvital.render.com/api";
-  const devUrl= "https://shiny-enigma-969r9vv6wj4wc7v4j-5000.app.github.dev/api";
-  if (process.env.NEXT_PUBLIC_IS_PRODUCTION) {
-    // In production environment, return the production backend URL
-    const productionURL = proUrl;
-    return productionURL;
-  } else {
-    // In development environment, return the development backend URL
-    const developmentURL = devUrl;
-    return developmentURL;
-  }
-}
-
-export const BackendURL = getBackendURLOnTheBasisOfEnvironment();
+// export const backendURL = "https://studious-meme-69p55wgq659rc44vw-5000.app.github.dev/api";
+export const backendURL = "https://xvital.render.com/api";
 
 
-// export const BackendURL = "https://studious-meme-69p55wgq659rc44vw-5000.app.github.dev/api";
+
