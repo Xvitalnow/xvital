c417c3a1ac4a338539f6366f0de0a5389203b516
Xvitalnow <xvitalnow@gmail.com>
2026-05-19 06:22:32 +0000

    fix: simplify production environment check in backend URL configuration

--- client/app/lib/config/url.js
+++ client/app/lib/config/url.js
@@ -1,7 +1,7 @@
 function getBackendURLOnTheBasisOfEnvironment() {
   const proUrl= "https://xvital.render.com/api";
   const devUrl= "https://shiny-enigma-969r9vv6wj4wc7v4j-5000.app.github.dev/api";
-  if (process.env.NEXT_PUBLIC_IS_PRODUCTION === "true") {
+  if (process.env.NEXT_PUBLIC_IS_PRODUCTION) {
     // In production environment, return the production backend URL
     const productionURL = proUrl;
     return productionURL;
