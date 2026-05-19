6662d76ac9f747bb80068d5f9899d91b3474698f
Xvitalnow <xvitalnow@gmail.com>
2026-05-19 06:52:29 +0000

    fix: correct backend URL in configuration

--- client/app/lib/config/url.js
+++ client/app/lib/config/url.js
@@ -1,5 +1,5 @@
 // export const BackendURL = "https://studious-meme-69p55wgq659rc44vw-5000.app.github.dev/api";
-export const BackendURL = "https://xvital.render.com/api";
+export const BackendURL = "https://xvital.onrender.com/api";
 
 
 
